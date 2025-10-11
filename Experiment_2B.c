4b7ab47945596780517588fdf18ee009d6d48ebf
arbaz09-fast <arbaz09@eng.rizvi.edu.in>
2025-10-11 23:43:15 +0530

    Update Experiment_2B.c

--- Experiment_2B.c
+++ Experiment_2B.c
@@ -7,10 +7,8 @@ UIN : 251M014
 int main()
 {   
         int num1, num2, i, sum = 0;
-
     printf("Enter first number: ");
     scanf("%d", &num1);
-
     printf("Enter second number: ");
     scanf("%d", &num2);
 
@@ -20,11 +18,7 @@ int main()
         if (i % 2 != 0) {
             printf("%d ", i);
             sum = sum + i;
-        
     }
-
     printf("\n\nSum of odd numbers between %d and %d = %d\n", num1, num2, sum);
-
-
     return 0;
 }
